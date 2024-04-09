7b734907b00fd512453af9c6da07828479ba0988
Isadora Garcia <isadoragarciaalmeidasilva7450@gmail.com>
2024-04-09 17:25:14 -0300

    feat: configurações iniciais de estilo

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
-function App() {
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
 
+function App() {
   return (
-    <h1>Hello</h1>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
 
